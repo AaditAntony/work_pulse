90ade4f06947af98173c86ce70e5074b1cd8edcb
Aadit Antony <aaditantony1112@gmail.com>
2026-05-25 16:32:20 +0530

    the attendence bloc is done

--- lib/features/attendance/bloc/attendance_bloc.dart
+++ lib/features/attendance/bloc/attendance_bloc.dart
@@ -1,27 +1,101 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:work_pulse/features/attendance/bloc/attendance_event.dart';
-import 'package:work_pulse/features/attendance/bloc/attendance_state.dart';
+
+import '../models/punch_model.dart';
+import 'attendance_event.dart';
+import 'attendance_state.dart';
 
 class AttendanceBloc extends Bloc<AttendanceEvent, AttendanceState> {
   AttendanceBloc() : super(const AttendanceState()) {
+
     on<OfficeInPressed>((event, emit) {
-      state.copyWith(status: AttendanceStatus.officeInCompleted);
+
+      if (state.status == AttendanceStatus.initial) {
+
+        final updatedPunches = List<PunchModel>.from(state.punches)
+          ..add(
+            PunchModel(
+              title: 'Office IN',
+              time: DateTime.now(),
+            ),
+          );
+
+        emit(
+          state.copyWith(
+            status: AttendanceStatus.officeInCompleted,
+            punches: updatedPunches,
+          ),
+        );
+      }
     });
+
     on<LunchOutPressed>((event, emit) {
-      emit(state.copyWith(status: AttendanceStatus.lunchOutCompleted));
+
+      if (state.status == AttendanceStatus.officeInCompleted) {
+
+        final updatedPunches = List<PunchModel>.from(state.punches)
+          ..add(
+            PunchModel(
+              title: 'Lunch OUT',
+              time: DateTime.now(),
+            ),
+          );
+
+        emit(
+          state.copyWith(
+            status: AttendanceStatus.lunchOutCompleted,
+            punches: updatedPunches,
+          ),
+        );
+      }
     });
 
     on<LunchInPressed>((event, emit) {
-      emit(state.copyWith(status: AttendanceStatus.lunchInCompleted));
+
+      if (state.status == AttendanceStatus.lunchOutCompleted) {
+
+        final updatedPunches = List<PunchModel>.from(state.punches)
+          ..add(
+            PunchModel(
+              title: 'Lunch IN',
+              time: DateTime.now(),
+            ),
+          );
+
+        emit(
+          state.copyWith(
+            status: AttendanceStatus.lunchInCompleted,
+            punches: updatedPunches,
+          ),
+        );
+      }
     });
 
     on<OfficeOutPressed>((event, emit) {
-      emit(state.copyWith(status: AttendanceStatus.officeOutCompleted));
+
+      if (state.status == AttendanceStatus.lunchInCompleted) {
+
+        final updatedPunches = List<PunchModel>.from(state.punches)
+          ..add(
+            PunchModel(
+              title: 'Office OUT',
+              time: DateTime.now(),
+            ),
+          );
+
+        emit(
+          state.copyWith(
+            status: AttendanceStatus.officeOutCompleted,
+            punches: updatedPunches,
+          ),
+        );
+      }
     });
 
     on<ResetAttendancePressed>((event, emit) {
-      emit(state.copyWith(status: AttendanceStatus.initial));
-    });
 
+      emit(
+        const AttendanceState(),
+      );
+    });
   }
 }
