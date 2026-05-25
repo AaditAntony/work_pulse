587739394825752c4f5d3448b125f910da64b6a2
Aadit Antony <aaditantony1112@gmail.com>
2026-05-25 16:03:37 +0530

    the code of the punches attendance state is done

--- lib/features/attendance/bloc/attendance_state.dart
+++ lib/features/attendance/bloc/attendance_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:work_pulse/features/attendance/models/punch_model.dart';
 
 enum AttendanceStatus {
   initial,
@@ -10,14 +11,24 @@ enum AttendanceStatus {
 
 class AttendanceState extends Equatable {
   final AttendanceStatus status;
+  final List<PunchModel> punches;
 
-  const AttendanceState({this.status = AttendanceStatus.initial});
+  const AttendanceState({
+    this.status = AttendanceStatus.initial,
+    this.punches = const [],
+  });
 
-  AttendanceState copyWith({AttendanceStatus? status}) {
-    return AttendanceState(status: status ?? this.status);
+  AttendanceState copyWith({
+    AttendanceStatus? status,
+    List<PunchModel>? punches,
+  }) {
+    return AttendanceState(
+      status: status ?? this.status,
+      punches: punches ?? this.punches,
+    );
   }
 
   @override
   // TODO: implement props
-  List<Object?> get props => [status];
+  List<Object?> get props => [status, punches];
 }
